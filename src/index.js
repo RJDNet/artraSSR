3a27baeef86fe1d97ea5cff4d850830ae9c9da7f
RJDNet <russdando86@gmail.com>
2020-02-08 17:24:02 +0000

    Updated URI

--- src/index.js
+++ src/index.js
@@ -33,7 +33,8 @@ app.get('*', async (req, res) => {
   const client = new ApolloClient({
     ssrMode: true,
     link: createHttpLink({
-      uri: 'http://cmtwebdash.herokuapp.com/graphql',
+      uri: 'https://cmtwebdash.herokuapp.com/graphql',
+      credentials: 'include',
       headers: {
         authorization: "cc5b60b7-a8fa-489d-84e6-a35d0b5cee6f"
       },
